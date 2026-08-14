e682abeaeee7e4b92e3d942a35406723566461c0
LEXONOS <louismcrenault@gmail.com>
2026-08-13 22:44:00 -0400

    Delete lib directory

--- lib/cn.ts
+++
@@ -1,9 +0,0 @@
-type ClassValue = string | false | null | undefined | ClassValue[];
-
-/** Concatène des classes conditionnelles. Pas de dépendance externe. */
-export function cn(...parts: ClassValue[]): string {
-  return parts
-    .flat(Infinity as 1)
-    .filter((p): p is string => typeof p === "string" && p.length > 0)
-    .join(" ");
-}
